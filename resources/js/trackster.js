0f3aef4693214aed6112afca7e52abb95a697422
Brad <br.brooks22@gmail.com>
2017-12-25 23:07:37 -0800

    Added sorting by list header in js

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
+  // search button input by clicking and the enter key
   $('#searchbutton').click(function() {
     Trackster.searchTracksByTitle($('.inputfield').val());
+    });
 
+  $('.inputfield').on('keydown', function(event) {
+    if (event.which === 13) {
+      Trackster.searchTracksByTitle($('.inputfield').val());
+    };
+  });
+
+  // Sorting the list table by 3 different headers
+  $('.songheader').click(function() {
+    trackList.sort(sortSongs);
+    $('#trackcontainer').empty();
+    $('#trackcontainer').append(trackList);
+  });
+
+  $('.artistheader').click(function() {
+    trackList.sort(sortArtists);
+    $('#trackcontainer').empty();
+    $('#trackcontainer').append(trackList);
+  });
+
+  $('.listenersheader').click(function() {
+    trackList.sort(sortListeners);
+    $('#trackcontainer').empty();
+    $('#trackcontainer').append(trackList);
   });
 });
 
@@ -19,18 +44,34 @@ Trackster.renderTracks = function(tracks) {
   for (var i = 0; i < tracks.length; i++) {
     var mediumAlbumArt = tracks[i].image[1]["#text"];
     var $trackrow = $("  <div class='container-fluid row' id='tracktable'>"
-     + "    <div class='col-xs-2 play'><a href='" + tracks[i].url +"'>"
+     + "    <div class='col-xs-1 play'><a href='" + tracks[i].url +"'>"
      + "    <i class='fa fa-play-circle-o fa-2x'></i></a></div>"
-     + "    <div class='col-xs-2 song'>" + tracks[i].name + "</div>"
+     + "    <div class='col-xs-4 song'>" + tracks[i].name + "</div>"
      + "    <div class='col-xs-4 artist'>" + tracks[i].artist + "</div>"
-     + "    <div class='col-xs-2 artwork'><img src='" + mediumAlbumArt +"'></div>"
-     + "    <div class='col-xs-2 listeners'>" + tracks[i].listeners + "</div>"
+     + "    <div class='col-xs-1 artwork'><img src='" + mediumAlbumArt +"'></div>"
+     + "    <div class='col-xs-1 listeners'>" + tracks[i].listeners + "</div>"
+     + "    <div class='col-xs-1 streamable'>" + tracks[i].streamable + "</div>"
      + "  </div>"
      + "</div>");
-      $('#trackcontainer').append($trackrow);
-
+     $('#trackcontainer').append($trackrow);
    };
+    window.trackList = $('#trackcontainer > #tracktable');
 };
+
+
+var sortSongs = function(a, b) {return a.children[1].textContent.localeCompare(
+  b.childNodes[3].textContent);
+};
+
+var sortArtists = function(a, b) {return a.children[2].textContent.localeCompare(
+  b.children[2].textContent);
+};
+
+var sortListeners = function(a, b) {return a.children[4].textContent -
+  b.children[4].textContent;
+};
+
+
 
 /*
   Given a search term as a string, query the LastFM API.
@@ -40,9 +81,19 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url:'http://ws.audioscrobbler.com/2.0/?method=track.search&track='
       + title + '&api_key=' + API_KEY + '&format=json',
+
+    beforeSend: function() {
+    $('header h1').css({'animation-name': 'headeranimation',
+    'animation-duration': '400ms'})
+      },
+
     success: function(response) {
         Trackster.renderTracks(response.results.trackmatches.track);
-      }
+      },
+
+    complete: function() {
+      $('header h1').css({'animation-name':''})
+    }
 
   });
 };
